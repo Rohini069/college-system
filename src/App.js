f8cc92f036a4f24e8addbf026e464c5572623691
Rohini <rohisk482@gmail.com>
2024-08-20 19:36:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Home from "./Components/Home";
+import Posts from "./Components/Posts";
+import DashBoard from "./Components/admin/DashBoard";
+import ProductsDetails from "./Components/ProductsDetails";
+import Products from "./Components/Products";
+import NotFound from "./Components/NotFound";
+import Login from "./Components/Forms/Login";
+import LoginSuccess from "./Components/Forms/LoginSuccess";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Navbar /> */}
+      <div className="content">
+        {/* <Switch>
+          <Route path="/products/:id" component={ProductsDetails} />
+          <Route path="/products" component={Products} />
+          <Route path="/posts/:year?/:month?" component={Posts} />
+          <Redirect from="/messages" to="/not-found" />
+          <Route path="/admin" component={DashBoard} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/not-found" />
+        </Switch> */}
+        <Redirect from="/" to="/login" />
+        <Route path="/login" component={Login} />
+        <Route path="/login-success" component={LoginSuccess} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
